cc4c8535ba964e59b4ae96a51faad68aa491ac23
dianamwhite <whi17003@byui.edu>
2023-01-21 15:25:02 +0100

    bi scr. cambios

---
+++ lesson3/scripts/main.js
@@ -0,0 +1,5 @@
+let year = new Date();
+document.querySelector('#year').textContent = year.getFullYear();
+
+let newdt = document.lastModified;
+    document.getElementById('lastupdated').innerHTML = newdt;
