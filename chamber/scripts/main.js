85e9bcf00496a211b6f29959b58c3096e0f648bb
dianamwhite <whi17003@byui.edu>
2023-01-28 00:45:58 +0100

    detail sizes

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -5,3 +5,16 @@ function toggleMenu(){
 }
  const x = document.getElementById("hambButton");
  x.onclick = toggleMenu;
+
+ //current date//
+const now = new Date();
+const fullDate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
+ document.querySelector(".date").innerHTML = fullDate;
+
+ //last updated//
+
+ let year = new Date();
+document.querySelector('#year').textContent = year.getFullYear();
+
+let newdt = document.lastModified;
+    document.getElementById('lastupdated').innerHTML = newdt;
