b87bc146126a67307f9c764e2b5c85be528c51b8
dianamwhite <whi17003@byui.edu>
2023-02-10 14:11:28 +0100

    fix DOM

--- lesson5/scripts/main.js
+++ lesson5/scripts/main.js
@@ -13,24 +13,27 @@ const list = document.querySelector('ul');
 //* Click event listener for the Add Chapter button using addEventListener and an anonymous function. *//
 
 button.addEventListener('click', () => {    //  =>  means function () //
-const chapter = input.value;
-input.value = '';
+    if (input.value !== ''){
+    const chapter = input.value;
+    input.value = '';
 
 const listItem = document.createElement('li');
 const listText = document.createElement('span');
 const deleteBtn = document.createElement('button');
 
-listText.textContent = chapter;
-listItem.appendChild(listText);
-deleteBtn.addEventListener('click', () => {
-list.removeChild(listItem);
+    listText.textContent = chapter;
+    listItem.appendChild(listText);
+
+    deleteBtn.addEventListener('click', () => {
+    list.removeChild(listItem);
 });
 
-deleteBtn.textContent = '❌';
-listItem.appendChild(deleteBtn);
+    deleteBtn.textContent = '❌';
+    listItem.appendChild(deleteBtn);
 
-list.appendChild(listItem);
+    list.appendChild(listItem);
 
-input.focus();
-
+    input.focus();
+    }
 });
+
