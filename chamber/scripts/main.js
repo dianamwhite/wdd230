2b99623e14cd419310c529fd15e2ec04dce70e2b
dianamwhite <whi17003@byui.edu>
2023-02-10 19:41:26 +0100

    .

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,3 +1,25 @@
+//current date and dates to use//
+const now = new Date();
+const fullDate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
+const day= now.getDate();
+
+ document.querySelector(".date").innerHTML = fullDate;
+
+//banner//
+
+    if (d.getDay() == 1) || (d.getDay() == 2) {
+    const banner= document.querySelector ('.banner');
+
+    //text banner//
+    banner.textContent= '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'; 
+
+    //style banner//
+    banner.style.backgroundColor = '#F4E8C1';
+    banner.style.color = '#3D3A30';
+    banner.style.textAlign = 'center';
+}
+
+//hamburguer Btn//
 function toggleMenu(){
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hambButton").classList.toggle("open");
@@ -6,11 +28,6 @@ function toggleMenu(){
  const x = document.getElementById("hambButton");
  x.onclick = toggleMenu;
 
- //current date//
-const now = new Date();
-const fullDate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
- document.querySelector(".date").innerHTML = fullDate;
-
  //last updated//
 
  let year = new Date();
@@ -18,3 +35,4 @@ document.querySelector('#year').textContent = year.getFullYear();
 
 let newdt = document.lastModified;
     document.getElementById('lastupdated').innerHTML = newdt;
+
