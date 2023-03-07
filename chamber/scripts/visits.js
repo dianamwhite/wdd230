1d3f1bf52fca424621234c0f3bcd87f1f0795c18
dianamwhite <whi17003@byui.edu>
2023-03-07 12:40:24 +0100

    .

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,8 +1,8 @@
 //local storage and stamp//
 
-
 const todayDisplay = document.querySelector("#today");
 const visitsDisplay = document.querySelector("#visits");
+const entrediasOutput = document.querySelector("#entredias");
 
  // get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls")); 
@@ -16,9 +16,25 @@ if (numVisits !== 0) {
 
 // increment in the number of visits.
 numVisits++;
+
 // store the new number of visits value
 localStorage.setItem("visits-ls", numVisits);
+
 // show todays date demonstration
-todayDisplay.textContent = Date.now();  //miliseconds in one day.
-let longnum= 1676650639658;
+todayDisplay.textContent = Date.now();
+//84600000 equals the number of miliseconds in one day.
+
+// Days since last time visit
 let today = new Date();
+let sincelastvisit = today.toString();
+document.getElementById('entredias').innerHTML = sincelastvisit;
+// check 
+
+//let entredias = (sincelastvisit.getTime() - Date.now()) / 84600000;
+//entrediasOutput.innerHTML = `${entredias.toFixed(0)} days`;
+
+
+
+
+
+
