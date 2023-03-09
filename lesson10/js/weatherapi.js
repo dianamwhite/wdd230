2fcf5bb147f802ec520ac75de398219bba4b2616
dianamwhite <whi17003@byui.edu>
2023-03-09 11:40:27 +0100

    weather

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -2,15 +2,15 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=9fc8c266a6745dbf6aa2518c3fc08bbd';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=9fc8c266a6745dbf6aa2518c3fc08bbd&units=imperial';
 
 async function apiFetch() {
     try {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // this is for testing the call
-        // displayResults(data);
+        //console.log(data); // this is for testing the call
+        displayResults(data);
       } else {
           throw Error(await response.text());
       }
@@ -32,6 +32,5 @@ function displayResults(weatherData) {
     //img attribute for the icon
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
-
     captionDesc.textContent = desc;
   };
